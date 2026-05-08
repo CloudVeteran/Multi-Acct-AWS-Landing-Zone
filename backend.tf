4b1f788afe9a6c71aa087084e6ff8f08cb4266c1
Marlon Garcia <cloud.veteran@proton.me>
2026-05-07 21:07:29 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-mg-may7"
-    key            = "landing-zone/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
