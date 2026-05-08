25cffff09d5d61049d76749fe6a5badcea3a4cb3
Marlon Garcia <cloud.veteran@proton.me>
2026-05-07 21:07:52 -0400

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "organization_id" {
-  value       = module.landing_zone.organization_id
-  description = "AWS Organization ID"
-}
-
-output "account_ids" {
-  value       = module.landing_zone.account_ids
-  description = "Map of all created account names to their IDs"
-}
